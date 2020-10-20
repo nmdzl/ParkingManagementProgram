622e06fc7e9651963a0dc4404f6b695ed9e27a31
rwu5 <wrwncsu@gmail.com>
2020-10-20 19:16:50 -0400

    fixing bug

--- csc540/src/jdbc/parking.java
+++ csc540/src/jdbc/parking.java
@@ -262,7 +262,7 @@ public class parking {
 	}
 	private static void addlot(String lname, String ladd, int ls, int lv, String lnotes) {
 		try {
-            rs = statement.executeQuery("INSERT INTO LOTS VALUES('" + lname + "','" + ladd + "'," + ls + "," + lv + ",'" + lnotes + "'" );
+            rs = statement.executeQuery("INSERT INTO LOTS VALUES('" + lname + "'" + ",'" + ladd + "'," + ls + "," + lv + ",'" + lnotes + "'" );
 		    System.out.println("Parking Lot " + lname + "successfully added.");
 		    System.out.println("----------------------------");
 		} catch (Exception e) {
