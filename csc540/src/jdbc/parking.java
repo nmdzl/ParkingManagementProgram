3d3738f3d3a3c402a59dffe598785adff86d33fa
rwu5 <wrwncsu@gmail.com>
2020-10-20 03:30:36 -0400

    Menu created

--- csc540/src/jdbc/parking.java
+++ csc540/src/jdbc/parking.java
@@ -8,6 +8,7 @@
 //package javaapplication5;
 import java.sql.*;
 import java.util.List;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -17,22 +18,174 @@ import java.io.Reader;
 public class parking {
 
 	static final String jdbcURL = "jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01";
+	private static BufferedReader reader = null;
+	private static Connection con = null;
+	private static Statement statement = null;
+	private static Scanner in;
 
 	public static void main(String[] args) {
 		try {
-			executeScriptUsingStatement();
+			connectDB();
+			dropAllTables();
+			setup();
+			//menu
+			in = new Scanner(System.in);
+	        boolean key = true;
+	        String s = "";
+	        while (key) {
+	        	System.out.println("#####################################################");
+	        	System.out.println("##                                                 ##");
+	        	System.out.println("##  Welcome to University Parking Service System!  ##");
+	        	System.out.println("##                                                 ##");
+	        	System.out.println("#####################################################\n");
+	        	System.out.println(" 0 - UPS Admin");
+	            System.out.println(" 1 - Employee");
+	            System.out.println(" 2 - Student");
+	            System.out.println(" 3 - Visitor");
+	            System.out.println(" 4 - Sample Queries ");
+	            System.out.println(" 5 - Reporting Queries ");
+	            System.out.println(" q - Exit System ");
+	            try {
+	                System.out.println("\nEnter number to perform actions: ");
+	                s = in.nextLine();
+	            } catch (Exception InputMismatchException ) {
+	                System.out.println("Invaild input, please try again");
+	            }
+	            if(s.equals("q")) {
+	            	key = false;
+	            }
+	            //admin
+	            if(s.equals("0")) {
+	    	        boolean k = true;
+	    	        String s1 = "";
+	    	        while (key) {
+	    	        	System.out.println("\n--------------------HELLO ADMIN!--------------------\n");
+	    	            System.out.println(" m - Return to Main Menu");
+	    	            try {
+	    	            	System.out.println("\nEnter number to perform actions: ");
+	    	            	s1 = in.nextLine();
+	    	            } catch (Exception InputMismatchException ) {
+	    	                System.out.println("Invaild input, please try again");
+	    	            }
+	    	            if(s1.equals("m")) {
+	    	            	k = false;
+	    	            	break;
+	    	            }
+	    	        }
+	            }
+	            
+	            //emp
+	            if(s.equals("1")) {
+	    	        boolean k = true;
+	    	        String s1 = "";
+	    	        while (key) {
+	    	        	System.out.println("\n--------------------HELLO EMP!--------------------\n");
+	    	            System.out.println(" m - Return to Main Menu");
+	    	            try {
+	    	            	System.out.println("\nEnter number to perform actions: ");
+	    	            	s1 = in.nextLine();
+	    	            } catch (Exception InputMismatchException ) {
+	    	                System.out.println("Invaild input, please try again");
+	    	            }
+	    	            if(s1.equals("m")) {
+	    	            	k = false;
+	    	            	break;
+	    	            }
+	    	        }
+	            }
+	            
+	            //student
+	            if(s.equals("2")) {
+	    	        boolean k = true;
+	    	        String s1 = "";
+	    	        while (key) {
+	    	        	System.out.println("\n--------------------HELLO STUDENT!--------------------\n");
+	    	            System.out.println(" m - Return to Main Menu");
+	    	            try {
+	    	            	System.out.println("\nEnter number to perform actions: ");
+	    	            	s1 = in.nextLine();
+	    	            } catch (Exception InputMismatchException ) {
+	    	                System.out.println("Invaild input, please try again");
+	    	            }
+	    	            if(s1.equals("m")) {
+	    	            	k = false;
+	    	            	break;
+	    	            }
+	    	        }
+	            }
+	            
+	            //visitor
+	            if(s.equals("3")) {
+	    	        boolean k = true;
+	    	        String s1 = "";
+	    	        while (key) {
+	    	        	System.out.println("\n--------------------HELLO VISITOR!--------------------\n");
+	    	            System.out.println(" m - Return to Main Menu");
+	    	            try {
+	    	            	System.out.println("\nEnter number to perform actions: ");
+	    	            	s1 = in.nextLine();
+	    	            } catch (Exception InputMismatchException ) {
+	    	                System.out.println("Invaild input, please try again");
+	    	            }
+	    	            if(s1.equals("m")) {
+	    	            	k = false;
+	    	            	break;
+	    	            }
+	    	        }
+	            }
+	            
+	            //sample
+	            if(s.equals("4")) {
+	    	        boolean k = true;
+	    	        String s1 = "";
+	    	        while (key) {
+	    	        	System.out.println("\n--------------------HELLO SAMPLE!--------------------\n");
+	    	            System.out.println(" m - Return to Main Menu");
+	    	            try {
+	    	            	System.out.println("\nEnter number to perform actions: ");
+	    	            	s1 = in.nextLine();
+	    	            } catch (Exception InputMismatchException ) {
+	    	                System.out.println("Invaild input, please try again");
+	    	            }
+	    	            if(s1.equals("m")) {
+	    	            	k = false;
+	    	            	break;
+	    	            }
+	    	        }
+	            }
+	            
+	            //report
+	            if(s.equals("5")) {
+	    	        boolean k = true;
+	    	        String s1 = "";
+	    	        while (key) {
+	    	        	System.out.println("\n--------------------HELLO REPORT!--------------------\n");
+	    	            System.out.println(" m - Return to Main Menu");
+	    	            try {
+	    	            	System.out.println("\nEnter number to perform actions: ");
+	    	            	s1 = in.nextLine();
+	    	            } catch (Exception InputMismatchException ) {
+	    	                System.out.println("Invaild input, please try again");
+	    	            }
+	    	            if(s1.equals("m")) {
+	    	            	k = false;
+	    	            	break;
+	    	            }
+	    	        }
+	            }
+	        }
+	        
+	        
+	        
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	static void connectDB() throws IOException, SQLException {
 
-	static void executeScriptUsingStatement() throws IOException, SQLException {
-		String scriptFilePath = "1.sql";
-		BufferedReader reader = null;
-		Connection con = null;
-		Statement statement = null;
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
 
@@ -45,14 +198,25 @@ public class parking {
 			con = DriverManager.getConnection(jdbcURL, user, passwd);
 			// create statement object
 			statement = con.createStatement();
-			final List<String> queries = SQLFileCache.getInstance().getQueries( "1.sql" );
-	        executeSQL( queries, con );
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+
+	static void setup() throws IOException, SQLException {
+
+		try {
+			final List<String> table = SQLFileCache.getInstance().getQueries( "table.sql" );
+	        executeSQL( table, con );
             ResultSet rs = null;
             rs = statement.executeQuery("SELECT table_name FROM user_tables");
     		while (rs.next()) {
     		    String s = rs.getString("table_name");
-    		    System.out.println(s);
+    		    System.out.println(s + " has been created!");
     		}
+    		final List<String> value = SQLFileCache.getInstance().getQueries( "value.sql" );
+	        executeSQL( value, con );
+	        System.out.println("Value inserted");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -110,4 +274,23 @@ public class parking {
 			}
 		}
 	}
+    public static void dropAllTables() {
+        try {
+            System.out.println("drop all existing Table");
+            //statement.executeUpdate("SET FOREIGN_KEY_CHECKS=0;");
+
+            statement.executeUpdate("DROP TABLE LOTS CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE ZONES CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE LHASZ CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE VEHICLES CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE VPERMITS CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE EPERMITS CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE NEPERMITS CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE EHASV CASCADE CONSTRAINTS");
+            statement.executeUpdate("DROP TABLE CITATION CASCADE CONSTRAINTS");
+            //statement.executeUpdate("SET FOREIGN_KEY_CHECKS=1;");
+        } catch (Throwable err) {
+            err.printStackTrace();
+        }
+    }
 }
