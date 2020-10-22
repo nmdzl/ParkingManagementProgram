7283f300f6fcf95ae9e7ba4f461a43aaecf52b45
rwu5 <wrwncsu@gmail.com>
2020-10-22 13:10:19 -0400

    11

--- csc540/src/jdbc/parking.java
+++ csc540/src/jdbc/parking.java
@@ -417,6 +417,7 @@ public class parking {
 								rs = statement.executeQuery(Q);
 								if (rs.next()) {
 									la = rs.getInt("TSPACE");
+									System.out.println("\nTotal Spaces: la");
 								}
 								else {
 									System.out.println("No such Parking Lot!");
@@ -1109,7 +1110,10 @@ public class parking {
 		}
 //    	String lname = "Premiere Lot";
 //    	String number = "200";
+		String q = "select category from SPACES where LOT='" + lname + "' AND sid =" + number;
+		System.out.println(q);
 		rs = statement.executeQuery("select category from SPACES where LOT='" + lname + "' AND sid =" + number);
+		
 		if (rs.next()) {
 			type = rs.getString("category");
 			int status = rs.getInt("sstatus");
